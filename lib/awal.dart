853a44a4efd363cbd750638655cbd3ade8df9a95
Ananda Fajar <nandafajarsaputra26@gmail.com>
2025-06-02 12:50:47 +0700

    main

---
+++ lib/awal.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Awal extends StatefulWidget {
+  const Awal({super.key});
+
+  @override
+  State<Awal> createState() => _AwalState();
+}
+
+class _AwalState extends State<Awal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+    );
+  }
+}
